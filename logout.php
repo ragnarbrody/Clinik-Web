30559f8fa8dc60cbf5311db1ef300ebf9734be8c
ragnarbrody <lcs.ldpais@gmail.com>
2023-09-18 21:23:54 -0300

    Atualização da documentação do código
    Criados comentários para auxiliar o entendimento do código.

--- logout.php
+++ logout.php
@@ -1,11 +1,14 @@
 <?php
-
+    //verifica se a sessão na pagina foi iniciada
     if(!isset($_SESSION))
     {
+        //inicia a sessão com os dados armazenados em cache
         session_start();
     }
 
+    //encerra a sessão do usuario, impedindo o mesmo de acessar ao sistema
     session_destroy();
 
+    //envia o usuário para a pagina de login
     header("Location: index.php");
 ?>
